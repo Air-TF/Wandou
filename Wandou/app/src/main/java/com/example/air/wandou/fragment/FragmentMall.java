bb0c6ffcf3018929aa664cdaccfdcdad542dfcb5
Air <1318860027@qq.com>
2017-08-06 02:04:18 +0800

    不知道什么鬼

--- Wandou/app/src/main/java/com/example/air/wandou/fragment/FragmentMall.java
+++ Wandou/app/src/main/java/com/example/air/wandou/fragment/FragmentMall.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import com.example.air.wandou.R;
 
 /**
  * Created by Air on 2017/8/1.
